d4faca587605bfbe0bcc6a15a16a0ed1c3d6d64d
Mayada <mayadayousuf96@gmail.com>
2022-10-13 16:58:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ put the string out there
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
