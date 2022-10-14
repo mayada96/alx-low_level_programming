6fc79c0491524e04aab46b629a082b70d9903e25
Mayada <mayadayousuf96@gmail.com>
2022-10-13 17:30:06 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entery
  *
- * return (0)
+ * Rreturn: (0) after printing
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf ("with proper grammar, but the outcome is a piece of art,");
 
 	return(0);
 }
