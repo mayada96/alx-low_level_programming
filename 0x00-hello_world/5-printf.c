d398a7777015d050e8f67231281981e2e763fb71
Mayada <mayadayousuf96@gmail.com>
2022-10-13 17:34:08 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entery
- *
- * Rreturn: (0) after printing
+ * print task
+ * Rreturn: 0 after printing
  */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 
 	return(0);
 }
