6d42a0f345b1b90236443e05db78f758f50639aa
Mayada <mayadayousuf96@gmail.com>
2022-10-15 06:07:45 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: return 0 when success
+ */
+
+int main(void)
+{
+	int n = 0;
+	
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		putchar(44);
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
