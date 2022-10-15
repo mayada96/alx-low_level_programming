95d9eed01ebb0d10d5becad056237628c7b3f3ec
Mayada <mayadayousuf96@gmail.com>
2022-10-14 19:18:34 -0700

    string outchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 when (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch = 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	return (0);
+}	
