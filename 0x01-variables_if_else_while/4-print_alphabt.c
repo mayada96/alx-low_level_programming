ba1781999e806a081486acdd71b309f79d8c5612
Mayada <mayadayousuf96@gmail.com>
2022-10-14 19:53:59 -0700

    No Q or E

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <+ 'z' ; ch++)
+	{
+		if (ch == 'q')
+			continue;
+		else if (ch == 'e')
+			continue;
+	putchar(ch);
+	}
+	putchar ('\n');
+	return (0);
+}
