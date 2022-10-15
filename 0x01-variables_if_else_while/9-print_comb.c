6bed0ffbbc85bf19755fb84010698c3ce14a7d27
Mayada <mayadayousuf96@gmail.com>
2022-10-15 08:22:08 -0700

    comma numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,17 @@
 int main(void)
 {
 	int n = 0;
-	int s = 9;
-	while (n <= 8)
+
+	while (n <= 9)
 	{
 		putchar(n + '0');
+		if (n < 9)
+		{
 		putchar(44);
 		putchar(' ');
+		}
 		n++;
 	}
-	putchar(s + '0');
+	putchar('\n');
 	return (0);
 }
