25a80d476933d068b4fe7d1801e418f89d194fc7
Mayada <mayadayousuf96@gmail.com>
2022-10-16 12:34:15 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 	int first_nums;
 	int second_nums;
 
-	for (first_nums = 0; first_nums < 100 ; first_nums++)
+	for (first_nums = 0 ; first_nums < 100 ; first_nums++)
 	{
-	for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
+	for (second_nums = first_nums + 1 ; second_nums < 100 ; second_nums++)
 		{
 			putchar (first_nums / 10 + '0');
 			putchar (first_nums % 10 + '0');
