73db9318a8636d44a57602e5d72ca3c9cc78bb76
Mayada <mayadayousuf96@gmail.com>
2022-10-18 16:15:42 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,10 @@ void print_to_98(int n)
 
 {
 	int i, j;
+
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)									
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
@@ -24,14 +25,14 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	}
-		       	else if (n >= 98)
-			{
-				for (j = n; j >= 98; j--)
-				{
-					if (j != 98)
-						printf("%d, ", j);
-					else if (j == 98)
-						printf("%d\n", j);
-				}
-			}
-}	
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
