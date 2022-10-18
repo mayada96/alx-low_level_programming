4e416b32de6403c4739e81c7941aec059c097b44
Mayada <mayadayousuf96@gmail.com>
2022-10-18 11:27:32 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower -check wether C is lowercase
+ *
+ * Return: 1 on success
+ * return: 0 atherwise
+ */
+
+int _islower(int c)
+{
+	return ( c >= 'a' && c <= 'z');
+}
