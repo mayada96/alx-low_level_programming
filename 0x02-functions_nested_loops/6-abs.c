c561801157f30a822b8f54743fd657178a933734
Mayada <mayadayousuf96@gmail.com>
2022-10-18 12:42:59 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs -Return the abslout value
+ * @a: the number to be print
+ *
+ * Return: 0 on success
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return(-a);
+	}
+	else
+	{
+	return (a);
+	}
+}
