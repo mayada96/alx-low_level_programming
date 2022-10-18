6aa2d03abebf1854330d568567908d2d5b18b6d8
Mayada <mayadayousuf96@gmail.com>
2022-10-18 13:27:00 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,17 @@
 
 int print_last_digit(int a)
 {
+	int n;
+	
 	if (a < 0)
 	{
 		a = -a;
 	}
-	a = a % 10;
-	_putchar(a + '0');
-	return (a);
+		n = a % 10;
+	if (n < 0)
+	{
+		n = -n;
+	}
+	_putchar(n + '0');
+	return (n);
 }
