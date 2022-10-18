efa6f068a22ef937314765cc8cfba2d10c7d0869
Mayada <mayadayousuf96@gmail.com>
2022-10-18 14:37:31 -0700

    24 Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,16 +10,17 @@ void jack_bauer(void)
 {
 	int a = 0;
 	int b;
+
 	while (a < 24)
 	{
 		b = 0;
 		while (a < 60)
 		{
 			_putchar((a / 10) + '0');
-			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
 			_putchar(':');
 			_putchar((a / 10) + '0');
-			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
 			_putchar('\n');
 			b++;
 		}
