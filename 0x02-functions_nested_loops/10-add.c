297f3322a5e7a75148f14928e81f1b868ade7774
Mayada <mayadayousuf96@gmail.com>
2022-10-18 15:09:36 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,16 @@
 
 /**
  * add - add 2 nums and retern result
- *@res: the addiction result
- * 
+ * @a: num 1 to be added
+ * @b: num 2 to be added
+ *
  * Return: the result
  */
 
 int add(int a, int b)
 {
 	int res;
-
+	
 	res = a + b;
 	return (res);
 }
