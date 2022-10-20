1638c88c0108d0aaef88378fe2d140bc78db25bb
Mayada <mayadayousuf96@gmail.com>
2022-10-20 01:11:49 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - couse an infinit loop
+ * 
  * Return:0
  */
 
@@ -12,12 +13,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 	while (i < 10)
 	{
 		putchar(i);
+		i++
 	}
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
