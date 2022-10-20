1f5dcadea48d3e370caa0bbfcc1712bd0a1afedd
Mayada <mayadayousuf96@gmail.com>
2022-10-20 03:16:19 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
  * don't delet it!
  * Return:0
  */
-
 int main(void)
 {
 	int i;
@@ -13,6 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	while (i < 10)
 	{
 		break;
