068de4b16f0356cacbe41d73c4e1da15c2a9c4cf
Mayada <mayadayousuf96@gmail.com>
2022-10-20 16:30:17 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - print uppercase char
+ * @c: char to be print
+ *
+ * Return: alphbet in uppercase
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
