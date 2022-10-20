1b3ef6780811e00fbd6ddafd4caf4556e280fcab
Mayada <mayadayousuf96@gmail.com>
2022-10-20 02:58:01 -0700

    the largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,19 +13,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if ((a > b && b > c) || (a > c && c > b))
 	{
 		largest = a;
 	}
-	else if (a > c && c > b)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (b > c && c > a)
+	else if ((b > a && a > c) || (b > c && c > a))
 	{
 		largest = b;
 	}
@@ -33,6 +25,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
