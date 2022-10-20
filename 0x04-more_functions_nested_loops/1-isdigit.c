ceebc525651cefaf7c11bbf138bd0bd43c8add7d
Mayada <mayadayousuf96@gmail.com>
2022-10-20 10:01:15 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - print the numbwrs
+ * @c: charecter to be intered
+ * Return: 1 on success
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
