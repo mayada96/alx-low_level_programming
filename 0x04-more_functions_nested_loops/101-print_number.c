56041da79d94224dbf62ff467883ff8dfe1b64aa
Mayada <mayadayousuf96@gmail.com>
2022-10-21 13:21:21 -0700

    Print the integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,38 +2,27 @@
 
 /**
  * print_number - print an integer number
- *@n:the integer number
+ * @n: the number to br printed
  *
- * Retern: integer number
+ * Return: void
  */
 
 void print_number(int n)
 {
+	int m;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -n;
 	}
-	if ((n / 10) != 0)
+	else 
 	{
-		if ((n / 10) >= 10 && (n / 10) < 100)
-		{
-			_putchar((n / 10) / 10 + '0');
-			_putchar((n / 10) % 10 + '0');
-		}
-		else if ((n / 10) >= 100)
-		{
-			if ((n / 10) / 10 != 0)
-			{
-				_putchar((((n / 10) / 10) / 10) + '0');
-				_putchar((((n / 10) / 10) % 10) + '0');
-			}
-			_putchar((n / 10) % 10 + '0');
-		}
-		else
-		{
-			_putchar((n / 10) + '0');
-		}
+		m = n;
 	}
-	_putchar((n % 10) + '0');
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
 }
