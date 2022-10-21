b69f3d26ff9438a29f7e4ec2b93ad2314c1c8e97
Mayada <mayadayousuf96@gmail.com>
2022-10-21 14:29:30 -0700

    primee

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,11 +15,18 @@ int main(void)
 	n = 612852475143;
 	for (i = 2 ; i <= n ; i++)
 	{
-		while (n % i == 0)
+		if (n % i != 0)
 		{
-			n = n / i;
+			continue;
+		}
+		else
+		{
+			while (n % i != 0)
+			{
+				printf("%ld", i);
+				n = n / i;
+			}
 		}
 	}
-	printf("%lu\n", n);
 	return (0);
 }
