b9b38a165c2e80b0188bca5ec585a5c7966b394a
Mayada <mayadayousuf96@gmail.com>
2022-10-26 12:20:01 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - to copy string one into string 2
+ * @dest: pointer to the first string
+ * @scr: pointer to the second empty string
+ * @n: bytes to be copied
+ *
+ * Return: the copy of the string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (*src && n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
