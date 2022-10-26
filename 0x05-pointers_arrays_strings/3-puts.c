0557c6add3de55fb98d902773fecba678a9ded9e
Mayada <mayadayousuf96@gmail.com>
2022-10-26 02:05:35 -0700

    puts string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,10 +9,13 @@
 
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar (*str);
-		str++;
+		_putchar(str[i]);
+		i++;
 	}
 
 	_putchar('\n');
