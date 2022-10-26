92b348928f351dca30767ba7e26bace39d5cc9a5
Mayada <mayadayousuf96@gmail.com>
2022-10-26 13:03:45 -0700

    Strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (*src && n--)
+	int i;
+
+	for (i = 0 ; src[i] != '\0' && i < n ; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
-	*dest = '\0';
+	for ( ; i < n ; i++)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 }
