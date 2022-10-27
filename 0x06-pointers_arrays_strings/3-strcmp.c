7c358e8e1ecd7c0fdf76da804e2dbb003936208f
Mayada <mayadayousuf96@gmail.com>
2022-10-27 02:09:26 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strcmp - compair two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: o on success compartion
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*s1)
+	{
+		if (*s2 == '\0')
+		{
+			i = i + 5;
+		}
+		else if (*s1 < *s2)
+		{
+			i = i - 5;
+		}
+		else if (*s1 == *s2)
+		{
+			i = i + 0;
+		}
+		else if (*s1 > *s2)
+		{
+			i = i + 5;
+		}
+		s1++;
+		s2++;
+	}
+	if (*s2 != '\0' && !*s1)
+	{
+		i = i - 5;
+	}
+	return (i);
+}
