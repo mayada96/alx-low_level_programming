e675c1edcc0dfa58aafd954f5f7e5fe3bf79106e
Mayada <mayadayousuf96@gmail.com>
2022-10-30 04:22:33 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,11 +16,11 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n <= 9)
+	if (n <= 9 && n >= 0)
 	{
 		_putchar(n + '0');
 	}
-	else if (n / 10)
+	else if (n / 10 != 0)
 	{
 		print_number(n / 10);
 		_putchar((n % 10) + '0');
