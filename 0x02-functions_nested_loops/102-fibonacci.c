387d2396a39ab6eee663054b14a3904e9f0c9b6b
Mayada <mayadayousuf96@gmail.com>
2022-10-30 07:14:55 -0700

    Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,25 @@
 
 int main(void)
 {
-	int i;
+	int i, n1;
+	int n3, n2;
 
-	for (i = 1 ; i <= 50 ; i++)
+	n1 = 1;
+	n2 = 2;
+	n3 = 0;
+	for (i = 3 ; i <= 50 ; i++)
 	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
 		if (i == 50)
 		{
-			printf("%d", i);
+			printf("%d", n3);
 			break;
 		}
 		else
 		{
-			printf("%d, ", i);
+			printf("%d, ", n3);
 		}
 	}
 	printf("\n");
