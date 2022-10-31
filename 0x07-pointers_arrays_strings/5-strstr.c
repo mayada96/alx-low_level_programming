ebd7a09669e8a16d2c3a30084a96e0e4b742d576
Mayada <mayadayousuf96@gmail.com>
2022-10-31 15:01:34 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - print string 1 starting from string 2
+ * @haystack: pointer to string 1
+ * @needle: pointer to string 2
+ *
+ * Return: string 1 starting for string 2 if found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *ph;
+	size_t n = strlen(needle);
+	
+	ph = haystack;
+	while (*ph)
+	{
+		if (!memcmp(ph, needle, n))
+		{
+			return (ph);
+		}
+		ph++;
+	}
+	return (NULL);
+}
