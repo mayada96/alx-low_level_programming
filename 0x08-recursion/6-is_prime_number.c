e974035b3ba364698cc1763b81c0159eafbbb7fc
Mayada <mayadayousuf96@gmail.com>
2022-11-02 15:12:46 -0700

    is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
 
 int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	if (n <= 2)
+	if (n == 2)
 	{
 		return (1);
 	}
@@ -21,5 +21,5 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return(is_prime_number(n - 1));
+	return (is_prime_number(n - 1));
 }
