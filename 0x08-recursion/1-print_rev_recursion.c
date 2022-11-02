f0dccbc7abcb63cbd21db7a81f38c704e86567c5
Mayada <mayadayousuf96@gmail.com>
2022-11-02 11:35:51 -0700

    revers string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,13 +9,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		i++;
 		_print_rev_recursion(s + 1);
-		i--;
 	}
-	_putchar(s[i]);
+	s--;
+	_putchar(*s);
 }
