415700e19f4e10c13f1ed78e698a3f570d5cd95a
Mayada <mayadayousuf96@gmail.com>
2022-11-04 16:23:03 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument string vector
+ *
+ * Return: the name of the file
+ */
+
+int main (int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
