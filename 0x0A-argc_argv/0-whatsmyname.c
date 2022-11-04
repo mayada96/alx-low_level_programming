9cdeef96e214cc2a4538fe26a86280f26aa40ba5
Mayada <mayadayousuf96@gmail.com>
2022-11-04 16:28:38 -0700

    My name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  * Return: the name of the file
  */
 
-int main (int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
