fd5fa298752d7b5b147eda31128d1a831c27eed3
Mayada <mayadayousuf96@gmail.com>
2022-11-04 16:40:42 -0700

    number of argus

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: count of arguments
+ * @argv: the string vector og arguments
+ *
+ * Return: the number of arguments pass to it
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
