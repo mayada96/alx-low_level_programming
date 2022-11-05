6df699fc726006df47abba225660f282cc17a963
Mayada <mayadayousuf96@gmail.com>
2022-11-04 18:01:34 -0700

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main -Entry point
+ * @argc: the arguments number
+ * @argv: the vector strinh
+ *
+ * Return: the sum of yhe enterd number og arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	int j;
+
+	if (argc > 1)
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			for (j = 0 ; argv[i][j] ; j++)
+			{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	else
+	{
+	printf("0\n");
+	}
+	return (0);
+}
