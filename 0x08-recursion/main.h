b4590a607149414e9861210fac77a7decb02a6dc
Mayada <mayadayousuf96@gmail.com>
2022-11-06 01:31:34 -0800

    plaindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
