8585c2cfd97beec7da10a134066aa061325efefd
Mayada <mayadayousuf96@gmail.com>
2022-11-09 13:14:48 -0800

    spilit string to words

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,12 +13,8 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-
-
-
-
-
-
-
+char **strtow(char *str);
+int word_len(char *str);
+int count_words(char *str);
 
 #endif
