a67a3495c37ed6e92ba95cd0780f08056bdc76df
Mayada <mayadayousuf96@gmail.com>
2022-11-14 12:36:19 -0800

    print dogyy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,13 +15,9 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
 	if (d->name == NULL)
 	{
-		d->name = "Name: (nil)";
+		d->name = "(nil)";
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
