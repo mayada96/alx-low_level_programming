4a02c6a5f528ccc7cc4b4b6380351dcc7c2c8d9b
Mayada <mayadayousuf96@gmail.com>
2022-12-05 12:05:29 -0800

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,8 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i] != '\0'; )
 			i++;
 	}
+	else
+		text_content = "";
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, i);
 	if (o == -1 || w == -1)
