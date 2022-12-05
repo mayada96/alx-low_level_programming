9cc0a9718e51adb6f4a53cad926af335524f75a0
Mayada <mayadayousuf96@gmail.com>
2022-12-05 12:00:17 -0800

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
  * @filename: the name of the file to be created
  * @text_content: a NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
