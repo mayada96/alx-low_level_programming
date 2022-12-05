c9bd12920cb6b929216b1da6af9274efdcbedd0a
Mayada <mayadayousuf96@gmail.com>
2022-12-05 12:11:13 -0800

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,10 @@ int create_file(const char *filename, char *text_content)
 	else
 		text_content = "";
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, i);
+	if (text_content != NULL)
+		w = write(o, text_content, i);
+	else
+		w = write(o, NULL, 0);
 	if (o == -1 || w == -1)
 		return (-1);
 	close(o);
