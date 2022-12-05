cff5fdd25464393229a599771fa1038d080c3fbc
Mayada <mayadayousuf96@gmail.com>
2022-12-05 11:58:39 -0800

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to be created
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure 
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, w, o;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; )
+			i++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, i);
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
